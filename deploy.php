1e5b762f9eb6996c2668d7ba7af671d98120a959
jochemth <jochemth@gmail.com>
2016-05-11 16:10:12 +0200

    Updated security yml

--- deploy.php
+++ deploy.php
@@ -5,6 +5,8 @@ localServer('localhost')
     ->user('deploy')
     ->env('deploy_path', '/var/www')
     ->env('branch', 'develop')
+    ->env('env_vars', 'SYMFONY_ENV=dev')
+    ->env('env', 'dev')
     ->stage('local');
 
 set('repository', 'git@github.com:jochemth/assignments.git');
