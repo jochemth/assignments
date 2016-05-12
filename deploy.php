46ef1a81a3c4216c487af8a95fd06e52963e8d39
jochemth <jochemth@gmail.com>
2016-05-12 10:56:48 +0200

    Updated composer and deploy script

--- deploy.php
+++ deploy.php
@@ -3,10 +3,20 @@ require 'recipe/symfony.php';
 
 localServer('localhost')
     ->user('deploy')
-    ->env('deploy_path', '/var/www')
+    ->env('deploy_path', '/var/www/beers')
     ->env('branch', 'develop')
     ->env('env_vars', 'SYMFONY_ENV=dev')
     ->env('env', 'dev')
     ->stage('local');
+
+server('vm', '192.168.0.144')
+    ->user('web')
+    ->identityFile()
+    ->forwardAgent()
+    ->env('deploy_path', '/home/sites/test')
+    ->env('branch', 'develop')
+    ->env('env_vars', 'SYMFONY_ENV=dev')
+    ->env('env', 'dev')
+    ->stage('vm');
 
 set('repository', 'git@github.com:jochemth/assignments.git');
